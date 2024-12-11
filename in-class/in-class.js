c99094f9224b242ae788ce92f426c53b601d2320
Sofia Shkidchenko <sofiashkidchenko@Sofias-MacBook-Pro-7.local>
2024-12-10 23:08:47 -0500

    trying?

---
+++ in-class/in-class.js
@@ -0,0 +1,39 @@
+// console.log("hi");
+
+// const pi = 3.1415
+
+// let x = 45
+// x=4
+
+
+// // single line comment
+
+// /* dksdj
+// sdfsjh
+// adhsjfh
+// */
+
+// let name = "Sofia"; // type = string
+// let age = 31; // type = number
+// let nothing = null;
+// let likesTea = true; //type = boolean
+
+// console.log (typeof(name))
+// console.log (typeof(null))
+// console.log (typeof(age))
+
+
+// let stringNum = '1'
+// let secondNum = 1;
+// let thirdNum = 4;
+// let fourthNum = 5;
+
+// // command+slash 
+
+// if (firstNum == secondNum && thirdNum > fourthNum);
+
+// if (firstNum == secondNum || thirdNum > fourthNum);
+
+const f = 99;
+c = (f-32)*5/9;
+console.log(c);
